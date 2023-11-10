020b6fac14f9d46567e18f1ec1f59b9eb0875315
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-11-10 02:34:22 +0100

    feat: search and filter functionality plus homepage completed

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,31 +1,42 @@
+import { Link } from "react-router-dom";
+
 interface CountryInfo {
   area: number;
   name: { common: string };
+  population: number;
+  region: string;
+  capital: string[];
   flags: { svg: string; alt: string };
 }
 
-function CountryCard({ countryObject }: { countryObject: CountryInfo }) {
+function CountryCard({ country }: { country: CountryInfo }) {
+  const allCapitals: string = country.capital?.join(", ");
+  // console.log(country.name.common.toLowerCase().includes("nicaragua"));
+
   return (
     <div className='min-h-[30rem] dark:bg-blue-700 self-center shadow-xl rounded-xl overflow-hidden'>
-      <img
-        src={countryObject.flags.svg}
-        alt={countryObject.flags.alt}
-        className='w-full min-h-[16rem] aspect-video object-cover'
-      />
+      <Link to={`/${country.name.common}`}>
+        <img
+          src={country.flags.svg}
+          alt={country.flags.alt}
+          className='w-full min-h-[16rem] aspect-video object-cover'
+        />
+      </Link>
 
       <div className='p-4 pb-10 flex flex-col gap-1'>
-        <h3 className='py-2 font-extrabold text-lg'>
-          {countryObject.name.common}
-        </h3>
+        <Link to={`/${country.name.common}`} className='hover:underline'>
+          <h3 className='py-2 font-extrabold text-lg'>{country.name.common}</h3>
+        </Link>
 
         <p className='text-base'>
-          <span className='font-semibold'>Population:</span> 81,770,900
+          <span className='font-semibold'>Population:</span>{" "}
+          {country.population.toLocaleString()}
         </p>
         <p className='text-base'>
-          <span className='font-semibold'>Region:</span> Europe
+          <span className='font-semibold'>Region:</span> {country.region}
         </p>
         <p className='text-base'>
-          <span className='font-semibold'>Capital:</span> Berlin
+          <span className='font-semibold'>Capital:</span> {allCapitals}
         </p>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,34 @@
 import { useState, useEffect } from "react";
 import CountryCard from "../components/CountryCard";
 import axios from "axios";
+import { SearchRounded } from "@mui/icons-material";
+
+interface CountryInfo {
+  area: number;
+  name: { common: string };
+  population: number;
+  region: string;
+  capital: string[];
+  flags: { svg: string; alt: string };
+}
 
 function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [countries, setCountries] = useState([]);
-  // const [region, setRegion] = useState<string>("all");
+  const [searchVal, setSearchVal] = useState("");
+  const [region, setRegion] = useState<string>("all");
 
   useEffect(() => {
-    // const url: string =
-    //   region === "all"
-    //     ? "https://restcountries.com/v3.1/all"
-    //     : `https://restcountries.com/v3.1/region/${region}`;
-    const url: string = "https://restcountries.com/v3.1/all";
+    let url: string;
+
+    if (searchVal) {
+      url = `https://restcountries.com/v3.1/name/${searchVal}`;
+    } else if (region !== "all") {
+      url = `https://restcountries.com/v3.1/region/${region}`;
+    } else {
+      url = "https://restcountries.com/v3.1/all";
+    }
+    // const url: string = "https://restcountries.com/v3.1/all";
 
     try {
       setIsLoading(true);
@@ -25,18 +41,58 @@ function Home() {
     } finally {
       setIsLoading(!isLoading);
     }
-  }, []);
+  }, [searchVal, region]);
 
-  console.log(countries);
+  // console.log(countries);
 
   return (
     <main className='min-h-[90vh] p-4 dark:bg-blue-900 dark:text-white'>
-      <div className='max-w-[124rem] mx-auto'>
-        <section className='w-full'>Section 1</section>
-        <section className='w-full grid grid-cols-[repeat(auto-fit,_minmax(25rem,1fr))] justify-center gap-10'>
-          {countries.map((country, index) => (
-            <CountryCard key={index} countryObject={country} />
-          ))}
+      <div className='max-w-[124rem] mx-auto pt-8 grid gap-8'>
+        <section className='w-full flex flex-wrap justify-between gap-2'>
+          <div className='relative w-full md:w-[45%]'>
+            <input
+              type='text'
+              value={searchVal}
+              onChange={(e) => setSearchVal(e.target.value)}
+              placeholder='Search for a country...'
+              className='dark:bg-blue-700 w-full p-3 pl-10 rounded-lg shadow-lg'
+            />
+            <SearchRounded
+              fontSize='large'
+              className='absolute left-3 top-1/2 -translate-y-1/2'
+            />
+          </div>
+
+          <select
+            name='region'
+            id='region'
+            value={region}
+            onChange={(e) => setRegion(e.target.value)}
+            className='dark:bg-blue-700 p-3 rounded-lg shadow-lg'
+          >
+            <option value='all'>Filter by Region</option>
+            <option value='africa'>Africa</option>
+            <option value='america'>America</option>
+            <option value='asia'>Asia</option>
+            <option value='europe'>Europe</option>
+            <option value='oceania'>Oceania</option>
+          </select>
+        </section>
+
+        <section className='w-full grid justify-start gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {searchVal
+            ? countries.map((countryInfo, index) => (
+                <CountryCard key={index} country={countryInfo} />
+              ))
+            : countries
+                .filter((countryInfo: CountryInfo) =>
+                  countryInfo.name.common
+                    .toLowerCase()
+                    .includes(searchVal.toLowerCase())
+                )
+                .map((countryInfo, index) => (
+                  <CountryCard key={index} country={countryInfo} />
+                ))}
         </section>
       </div>
     </main>
